b62c10bfc029d0d5841ba14e65f7d7f86f973897
jar17 <31600580+jar17@users.noreply.github.com>
2017-10-27 18:14:51 -0600

    initial commit of reqs

--- gamechanger.py
+++ gamechanger.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python
 
+from __future__ import absolute_import, print_function
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from time import sleep
+import dataset
+import re
+import praw
+import twittercfg as cfg
+from sqlalchemy.exc import ProgrammingError
+
+
+
