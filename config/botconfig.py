219db4631536b6a1043d65849a80950a55038b8a
jar17 <31600580+jar17@users.noreply.github.com>
2017-10-28 15:34:08 -0600

    changed some formatting

--- config/botconfig.py
+++ config/botconfig.py
@@ -1,1 +1,6 @@
+slack_creds = {'api_token' : 'xoxb-262799913601-TZNeTtS8k31rdJepzTWhgxqP'}
 
+twitter_creds = {'consumer_key': 'OR1MYJMl04pvpf6KaIZaMmwiD',
+				 'consumer_secret': 'HXXKOtmjzi3WItluQo519OQxNcuv8lFScWud636soXSWyhGTUg',
+				 'access_token': '1020727524-byTxGlqHyL4BlpmkjHqT36lGm9jLYlmZlZCAIEY',
+				 'access_token_secret': 'tKjv8QEISYFMw2yybwDJ3VatuZxXDntD38d7AeOIK4TDy'}
