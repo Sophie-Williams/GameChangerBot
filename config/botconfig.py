2ba1ee19f8615585ac90c79215420abd7d3dde83
jar17 <31600580+jar17@users.noreply.github.com>
2017-10-28 17:00:59 -0600

    Update botconfig.py

--- config/botconfig.py
+++ config/botconfig.py
@@ -1,6 +1,1 @@
-slack_creds = {'api_token' : 'xoxb-262799913601-TZNeTtS8k31rdJepzTWhgxqP'}
 
-twitter_creds = {'consumer_key': 'BEGVt69eddF5UWVbaofKlEVT5',
-				 'consumer_secret': 'ufF3F3lUAQnTgZtjGHzmx3oJxOy7dj59jcQQVdP8lriy4w563K',
-				 'access_token': '924389647861432320-iKlvxwCxSnA6Xamw9IJzmbC1QnRAW5Q',
-				 'access_token_secret': 'IPq0VvgFrct1xIFJqI1hEMbVLcKJi6TaecLagrDzszf1a'}
